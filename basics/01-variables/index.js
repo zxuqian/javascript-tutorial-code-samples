9cdd29d45d2c81f19dcdac5256f2e178ac661cf4
zxuqian <zxuqian@gmail.com>
2020-02-10 21:42:11 +0800

    add intro code samples

---
+++ basics/01-variables/index.js
@@ -0,0 +1,17 @@
+var name = "峰华";
+
+var fontColor = "red";
+var font_color = "white";
+
+console.log(name);
+console.log(fontColor);
+console.log(font_color);
+
+name = "马云";
+
+var a = 5;
+var b = 6;
+
+var c = a + b;
+
+console.log(c + 5);
